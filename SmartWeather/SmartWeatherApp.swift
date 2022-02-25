c90d605788d843d4d69797cc9b4224b19c8e5a03
Alex <a>
2022-02-25 11:04:53 +0600

    Initial Commit

---
+++ SmartWeather/SmartWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartWeatherApp.swift
+//  SmartWeather
+//
+//  Created by 🌚🌚 Царь🌚🌚 on 25.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
